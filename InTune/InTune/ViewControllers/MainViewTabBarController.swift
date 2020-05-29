7728e39e561e675cccc486cb6deb6de37b76429f
Tiffany Obi <tiffanyobi123@gmail.com>
2020-05-29 18:28:02 -0400

    configured tab bar programatically

--- InTune/InTune/ViewControllers/MainViewTabBarController.swift
+++ InTune/InTune/ViewControllers/MainViewTabBarController.swift
@@ -10,21 +10,41 @@ import UIKit
 
 class MainViewTabBarController: UITabBarController {
 
+    lazy var exploreVC:ExploreViewController = {
+        let vc = ExploreViewController()
+        vc.tabBarItem = UITabBarItem(title: "Explore", image: UIImage(systemName: "globe"), tag: 0)
+        return vc
+    }()
+    
+    lazy var likedArtistsVC:LikedArtistsViewController = {
+        let vc = LikedArtistsViewController()
+        vc.tabBarItem = UITabBarItem(title: "Liked Artists", image: UIImage(systemName: "bookmark.fill"), tag: 1)
+        return vc
+    }()
+    
+    lazy var messengerVC:MessengerViewController = {
+        let vc = MessengerViewController()
+        vc.tabBarItem = UITabBarItem(title: "Chat", image: UIImage(systemName: "message.fill"), tag: 2)
+        return vc
+    }()
+    
+    lazy var profileVC:ProfileViewController = {
+        
+        let storyboard = UIStoryboard(name: "MainView", bundle: nil)
+        guard let viewController =  storyboard.instantiateViewController(identifier: "ProfileViewController") as? ProfileViewController else { return ProfileViewController()}
+        viewController.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person.fill"), tag: 3)
+        return viewController
+        
+       }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //loadTopics()
+        let controllers = [exploreVC,likedArtistsVC,messengerVC,profileVC]
+        viewControllers = controllers.map{UINavigationController(rootViewController: $0)}
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
