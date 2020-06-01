6152ef6964767b7c24b415ca1fa0ef024a312c3c
oscar victoria <oscarvictoriagonzalez@gmail.com>
2020-06-01 14:15:44 -0400

    upaded message view

---
+++ InTune/InTune/Models/Chat.swift
@@ -0,0 +1,35 @@
+//
+//  Chat.swift
+//  InTune
+//
+//  Created by Oscar Victoria Gonzalez  on 6/1/20.
+//  Copyright © 2020 Tiffany Obi. All rights reserved.
+//
+
+import UIKit
+
+struct Chat {
+    
+    var users: [String]
+    
+    var dictionary: [String: Any] {
+        return [
+            "users": users
+        ]
+    }
+}
+
+extension Chat {
+    
+    init?(dictionary: [String:Any]) {
+        guard let chatUsers = dictionary["users"] as? [String] else {return nil}
+        self.init(users: chatUsers)
+    }
+    
+}
+
+
+struct Contacts {
+    let name: String
+    let id: String
+}
