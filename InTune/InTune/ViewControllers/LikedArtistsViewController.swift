7dadf133cc98dcc33c5f7542992061ce55834326
Tiffany Obi <tiffanyobi123@gmail.com>
2020-06-04 13:51:13 -0400

    added artist cell for liked artist collection view

--- InTune/InTune/ViewControllers/LikedArtistsViewController.swift
+++ InTune/InTune/ViewControllers/LikedArtistsViewController.swift
@@ -18,17 +18,19 @@ class LikedArtistsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGroupedBackground
-       
-
+       setUpCollectionView()
+        likedArtistView.likedArtistCollectionView.register(UINib(nibName: "ArtistCell", bundle: nil), forCellWithReuseIdentifier: "artistCell")
     }
     
     private func setUpCollectionView(){
         
         likedArtistView.likedArtistCollectionView.dataSource = self
         likedArtistView.likedArtistCollectionView.delegate = self
-        likedArtistView.contactsCollectionView.dataSource = self
-        likedArtistView.contactsCollectionView.delegate = self
+//        likedArtistView.contactsCollectionView.dataSource = self
+//        likedArtistView.contactsCollectionView.delegate = self
         
+        
+//        likedArtistView.contactsCollectionView.register(ArtistCell.self, forCellWithReuseIdentifier: "artistCell")
 
     }
 
@@ -42,13 +44,22 @@ extension LikedArtistsViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-   let cell = UICollectionViewCell()
-        return cell
+        
+        guard let artistCell = collectionView.dequeueReusableCell(withReuseIdentifier:"artistCell", for: indexPath) as? ArtistCell else {
+            fatalError("Could not downcast to ArtistCell")
+        }
+        
+        return artistCell
     }
     
     
 }
 
 extension LikedArtistsViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let maxSize:CGSize = UIScreen.main.bounds.size
+        let itemWidth:CGFloat = maxSize.width * 0.4
+        return CGSize(width: itemWidth, height: collectionView.bounds.size.height * 0.3)
+    }
     
 }
---
+++ InTune/InTune/Views/ArtistCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArtistCell.swift
+//  InTune
+//
+//  Created by Tiffany Obi on 6/4/20.
+//  Copyright © 2020 Tiffany Obi. All rights reserved.
+//
+
+import UIKit
+
+class ArtistCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var displayNameLabel: UILabel!
+    
+}
