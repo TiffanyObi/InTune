07ae4e4b07009d8070b4e5f8c030fa8c4a797726
Tiffany Obi <tiffanyobi123@gmail.com>
2020-06-03 23:19:26 -0400

    refactored loginVC to include LoginViewModel - mvvm

--- InTune/InTune/Models/FavoriteVideo.swift
+++ InTune/InTune/Models/FavoriteVideo.swift
@@ -6,13 +6,76 @@
 //  Copyright © 2020 Tiffany Obi. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
-struct FavoritedVideo {
-    // Note: We decided to stick with favoriting an Artist instead of a video since artists will be limited to 4 videos.
-    //but i dont want to delete or rename/move it until we have another use for it. 
-}
-
-extension FavoritedVideo{
+struct LoginViewViewModel {
     
-}
+    private var authSession = AuthenticationSession()
+    private var accountState: AccountState = .existingUser
+    
+    var loginButtonTitle: String {
+        return "LOGIN"
+    }
+    var signUpButtonTitle:String {
+        return "SIGN UP"
+    }
+    
+    var newAccountMessage:String{
+        return "Don't have an account?"
+    }
+    
+    var exisistingAccountMessage: String {
+        return "Already have an account ?"
+    }
+    var missingFields:String {
+        return "Missing Fields"
+    }
+    var missingFieldsMessage:String {
+        return "Please fill all missing fields"
+    }
+    var setErrorLabelToEmpty:String{
+        return ""
+    }
+    func loginFlow(email:String,password:String) {
+        if accountState == .existingUser {
+            authSession.signExistingUser(email: email, password: password) { (result) in
+                           switch result {
+                           case .failure(let error):
+                               print(error)
+                               DispatchQueue.main.async {
+                                LoginViewController().errorMessageLabel.isHidden = false
+                                LoginViewController().errorMessageLabel.text = "Incorrect Login: \(error.localizedDescription)"
+                                LoginViewController().errorMessageLabel.textColor = .white
+                               }
+                           case .success:
+                               DispatchQueue.main.async {
+                                   //navigate to main view
+                                self.navigateToMainView()
+                               }
+                           }
+                       }
+                   } else {
+                    
+                        authSession.createNewUser(email: email, password: password) { (result) in
+                            switch result {
+                            case .failure(let error):
+                                print(error)
+                                DispatchQueue.main.async {
+                                    LoginViewController().errorMessageLabel.isHidden = false
+                                    LoginViewController().errorMessageLabel.text = "Error: \(error.localizedDescription)"
+                                    LoginViewController().errorMessageLabel.textColor = .white
+                                }
+                            case .success(let authDataResult):
+                                print(authDataResult.user.email ?? "user email")
+     
+                            }
+                        }
+                    }
+                   
+                }
+        
+    private func navigateToMainView() {
+        UIViewController.showViewController(storyboardName: "MainView", viewControllerID: "MainViewTabBarController")
+    }
+    }
+
