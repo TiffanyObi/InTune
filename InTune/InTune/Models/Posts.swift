7190184d79b357a46ae2b69eb0d2689c241506b9
maitreebain <maitreebain@gmail.com>
2020-06-10 11:58:02 -0400

    added gigs post model

--- InTune/InTune/Models/Posts.swift
+++ InTune/InTune/Models/Posts.swift
@@ -37,5 +37,36 @@ extension ArtistPost {
 
 //for gigs
 struct GigsPost {
-    
+    let user: Artist
+    //based on bool
+    let gigId: String
+    let title: String
+    let descript: String
+    let photoURL: String
+    let price: Int
+    let eventDate: String
+    let createdDate: Timestamp
+}
+
+extension GigsPost {
+    init?(_ dictionary: [String: Any]) {
+        guard let user = dictionary["user"] as? Artist,
+        let gigId = dictionary["gigId"] as? String,
+        let title = dictionary["title"] as? String,
+        let descript = dictionary["descript"] as? String,
+        let photoURL = dictionary["photoURL"] as? String,
+        let price = dictionary["price"] as? Int,
+        let eventDate = dictionary["eventDate"] as? String,
+            let createDate = dictionary["createdDate"] as? Timestamp else {
+                return nil
+        }
+        self.user = user
+        self.gigId = gigId
+        self.title = title
+        self.descript = descript
+        self.photoURL = photoURL
+        self.price = price
+        self.eventDate = eventDate
+        self.createdDate = createDate
+    }
 }
