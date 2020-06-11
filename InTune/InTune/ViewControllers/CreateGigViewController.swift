521014094e4cbacbec416a366b271ad6334864b0
oscar victoria <oscarvictoriagonzalez@gmail.com>
2020-06-11 18:02:09 -0400

    updated UI for gigs

---
+++ InTune/InTune/ViewControllers/CreateGigViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CreateGigViewController.swift
+//  InTune
+//
+//  Created by Oscar Victoria Gonzalez  on 6/11/20.
+//  Copyright © 2020 Tiffany Obi. All rights reserved.
+//
+
+import UIKit
+
+class CreateGigViewController: UIViewController {
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var gigImageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+    
+    
+    
+
+
+}
